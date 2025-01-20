8348ef26793531cddcc4a09a720c19fabbf298b4
techleadhd <61847557+techleadhd@users.noreply.github.com>
2025-01-20 14:55:49 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,142 @@
+import {
+  createFungible,
+  mplTokenMetadata,
+} from '@metaplex-foundation/mpl-token-metadata'
+import {
+  AuthorityType,
+  createTokenIfMissing,
+  findAssociatedTokenPda,
+  getSplAssociatedTokenProgramId,
+  mintTokensTo,
+  setAuthority,
+  setComputeUnitPrice,
+} from '@metaplex-foundation/mpl-toolbox'
+import {
+  generateSigner,
+  percentAmount,
+  keypairIdentity,
+  transactionBuilder,
+} from '@metaplex-foundation/umi'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { irysUploader } from '@metaplex-foundation/umi-uploader-irys'
+import { base58 } from '@metaplex-foundation/umi/serializers'
+import fs from 'fs'
+
+// Upload 'image.jpg' to Arweave.
+const uploadImage = async () => {
+  const umi = loadUmi();
+
+  console.log("Uploading image to Arweave");
+  const imageFile = fs.readFileSync("./image.jpg");
+  console.log("Upload price", await umi.uploader.getUploadPrice([imageFile]));
+  const imageUris = await umi.uploader.upload([imageFile]);
+  console.log(imageUris);
+};
+
+// Upload metadata to Arweave.
+const uploadMetadata = async (imageUri) => {
+  const umi = loadUmi();
+
+  const metadata = {
+    name: "TechLead",
+    symbol: "TL",
+    description: "",
+    image: imageUri,
+    "website": "https://www.youtube.com/techlead",
+    "twitter": "https://x.com/techleadhd",
+  };
+  console.log("Uploading metadata to Arweave");
+  const uri = await umi.uploader.uploadJson(metadata).catch((err) => {
+    throw new Error(err);
+  });
+  console.log(uri);
+};
+
+const mintToken = async (imageUri, metadataUri) => {
+  const umi = loadUmi();
+
+  // Mint the token.
+  let numDecimals = 6;
+  const totalSupply = 1000000 * Math.pow(10, numDecimals);
+
+  const mintSigner = generateSigner(umi);
+  const createFungibleIx = createFungible(umi, {
+    mint: mintSigner,
+    name: "TechLead",
+    symbol: "TL",
+    uri: metadataUri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: numDecimals,
+    isMutable: 0,
+  });
+
+  // This instruction will create a new Token Account if required, if one is found then it skips.
+  const createTokenIx = createTokenIfMissing(umi, {
+    mint: mintSigner.publicKey,
+    owner: umi.identity.publicKey,
+    ataProgram: getSplAssociatedTokenProgramId(umi),
+  });
+
+  // The final instruction (if required) is to mint the tokens to the token account in the previous ix.
+  const mintTokensIx = mintTokensTo(umi, {
+    mint: mintSigner.publicKey,
+    token: findAssociatedTokenPda(umi, {
+      mint: mintSigner.publicKey,
+      owner: umi.identity.publicKey,
+    }),
+    amount: BigInt(totalSupply),
+  });
+
+  // The last step is to send the ix's off in a transaction to the chain.
+  console.log("Sending transaction")
+  const tx = await
+    transactionBuilder()
+      .add(setComputeUnitPrice(umi, { microLamports: 1000000 }))
+      .add(createFungibleIx)
+      .add(createTokenIx)
+      .add(mintTokensIx)
+      .add(setAuthority(umi, {
+        owned: mintSigner.publicKey,
+        owner: umi.identity.publicKey,
+        authorityType: AuthorityType.MintTokens,
+        newAuthority: null
+      }))
+      .add(setAuthority(umi, {
+        owned: mintSigner.publicKey,
+        owner: umi.identity.publicKey,
+        authorityType: AuthorityType.FreezeAccount,
+        newAuthority: null
+      }))
+      .sendAndConfirm(umi);
+
+  const signature = base58.deserialize(tx.signature)[0];
+  console.log('\nTransaction Complete')
+  console.log('View Transaction on Solana Explorer')
+  console.log(`https://solscan.io/tx/${signature}`)
+  console.log('View Token on Solana Explorer')
+  console.log(`https://solscan.io/token/${mintSigner.publicKey}`)
+};
+
+const loadUmi = () => {
+  const umi = createUmi('https://api.mainnet-beta.solana.com')
+    .use(mplTokenMetadata())
+    .use(irysUploader())
+
+  const walletFile = fs.readFileSync('./keypair.json')
+  let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(
+    JSON.parse(walletFile)));
+  umi.use(keypairIdentity(keypair));
+  console.log("Using wallet", umi.identity.publicKey);
+  return umi;
+};
+
+// Step 1: Upload image
+// const imageUri = await uploadImage();
+// const imageUri = 'https://arweave.net/uv3P7skZVsJtJVHa2N64OXBafkFmSFJY23TSKj9-_gE';
+
+// Step 2: Upload metadata
+// const metadataUri = await uploadMetadata(imageUri);
+// const metadataUri = 'https://arweave.net/vFkLt4NG-kVE3INckdkxWWi5Y5HRZkaGvToXJ7rOsoU';
+
+// Step 3: Mint token
+// await mintToken(imageUri, metadataUri);
